6c7c5bef5e24eeab27f2fe3aa95206e5b2137364
hawrot <mhawro200@caledonian.ac.uk>
2020-05-25 21:47:44 +0100

    Add two way binding for the title and amount

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -5,7 +5,7 @@ import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
 
-  useState({
+ const inputState = useState({
     title: '',
     amount: ''
   });
@@ -21,11 +21,11 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title: event.target.value})} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount: event.target.value})} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
