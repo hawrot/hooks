e61db12d4743053fdb2412b84270a390febc101a
hawrot <mhawro200@caledonian.ac.uk>
2020-05-25 23:37:23 +0100

    Add loading indicator

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,11 +2,13 @@ import React, {useState, useEffect, useCallback} from 'react';
 import IngredientList from "./IngredientList";
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import LoadingIndicator from "../UI/LoadingIndicator";
 
 
 const Ingredients = () => {
 
     const [ingredients, setIngredients] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
 
     const filteredIngredientsHandler = useCallback(filteredIngredients => {
@@ -14,11 +16,13 @@ const Ingredients = () => {
     }, []);
 
     const addIngredientHandler = ingredient => {
+        setIsLoading(true);
         fetch(process.env.REACT_APP_FIRE_API + 'ingredients.json', {
             method: 'POST',
             body: JSON.stringify(ingredient),
             headers: {'Content-Type': 'application/json'}
         }).then(res => {
+            setIsLoading(false);
             return res.json();
 
         }).then(resData => {
@@ -37,8 +41,11 @@ const Ingredients = () => {
 
     }
 
+    const loading = isLoading ? <LoadingIndicator/> : null;
+
     return (
         <div className="App">
+            {loading}
             <IngredientForm onAddIngredient={addIngredientHandler}/>
             <section>
                 <Search onLoadIngredients={filteredIngredientsHandler}/>
