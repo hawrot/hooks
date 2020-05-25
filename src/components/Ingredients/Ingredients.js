68bbb2b90ca3f9a121b9bd77a822df886309ca08
hawrot <mhawro200@caledonian.ac.uk>
2020-05-25 22:44:47 +0100

    Send ingredients into firebase db

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -9,8 +9,18 @@ const Ingredients = () => {
     const [ingredients, setIngredients] = useState([]);
 
     const addIngredientHandler = ingredient => {
-        setIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}
-        ]);
+        fetch(process.env.REACT_APP_FIRE_API + 'ingredients.json', {
+            method: 'POST',
+            body: JSON.stringify(ingredient),
+            headers: {'Content-Type': 'application/json'}
+        }).then(res => {
+            return res.json();
+
+        }).then(resData => {
+            setIngredients(prevIngredients => [...prevIngredients, {id: resData.name, ...ingredient}
+            ])
+        });
+
     }
 
     const removeIngredientHandler = ingredientId => {
@@ -20,7 +30,6 @@ const Ingredients = () => {
     return (
         <div className="App">
             <IngredientForm onAddIngredient={addIngredientHandler}/>
-
             <section>
                 <Search/>
                 <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler}/>
